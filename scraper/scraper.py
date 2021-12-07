a5f5550674cc12d9b169a8c8d9208c2f5ee9a3ef
Dharmik Chaklasiya <20imc11134@fh-krems.ac.at>
2021-12-08 00:46:41 +0100

    scraper updated with Ahmed's scraper_2.py

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -6,7 +6,115 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import os
 import re
+import string
+import json
+import sys
+from SQLite_Db.ChessGameDB import getChessDb
+
+def color_parser(string:str) -> str:
+    yellow = "color: rgb(192, 149, 38)"
+    blue = "color: rgb(65, 133, 191)"
+    red = "color: rgb(191, 59, 67)"
+    green = "color: rgb(78, 145, 97)"
+
+    if yellow in string:
+        return "Yellow"
+    elif blue in string:
+        return "Blue"
+    elif green in string:
+        return "Green"
+    elif red in string:
+        return "Red"
+    else:
+        return "No color specified"
+
+#i suggest doing scrapping in test.py at the moment because scrapper is all in one script and every time you run it will open chrome fro chess.com login
+def scraper():
+    path = os.getcwd()
+    x = os.path.abspath(os.path.join(path, os.pardir))
+    sys.path.insert(1, x + "\\SQLite_Db")
+    # print(x)
+    print(os.getcwd())
+
+    #with open("data/pages/page.html", "a", encoding="UTF-8") as f:
+    html = driver.page_source
+    #in loop we can accsess directly html in soup
+    #f.write(html)
+    soup = BeautifulSoup(html, 'html.parser')
+    #f.write(soup.prettify())
+    # Finding HTML code with GameNr
+    GameNr = soup.find("span", {"class": "chat-content-f infomsg"})
+    # Analysing and finding the GameNr with Regex
+    regex1 = re.search(r'Game #(\d+)', str(GameNr))
+    # Getting only the Number
+    regex2 = regex1.group(1)
+    # print(regex2)
+
+    # dictionary
+    jsonString = {}
+    jsonString[regex2] = []
+
+    try:
+        for i in soup.find_all("div", {"class": "tableRow"}):
+
+            roundNr = i.div.text
+            color = i.find_all("div", {"class": "pointer"})
+            usernames = soup.find_all("div", {"class": "truncate120"})
+            player_name = [i.text.split('\n')[2].translate({ord(c): None for c in string.whitespace}) for i in usernames]
+
+            # print(f"\nRound Number: {roundNr.strip()}\n")
+
+            for j in color:
+
+                if color_parser(j.span['style']) == "Red":
+
+                    # Easier to read every information
+                    jsonString[regex2].append({
+                        'username': player_name[0],
+                        'round_nr': roundNr.split()[0],
+                        'moves-time': j['title'],
+                        'color': 'Red'
+                    })
+                    # print(f"Player: {player_name[0]}|{color_parser(j.span['style'])}|\n\t Moves:\n\t{j['title']}")
+
+                elif color_parser(j.span['style']) == "Blue":
+
+                    # Easier to read every information
+                    jsonString[regex2].append({
+                        'username': player_name[1],
+                        'round_nr': roundNr.split()[0],
+                        'moves-time': j['title'],
+                        'color': 'Blue'
+                    })
+
+                    # pr}|{color_paint(f"Player: {player_name[1]rser(j.span['style'])}|\n\t Moves:\n\t{j['title']}")
+
+                elif color_parser(j.span['style']) == "Yellow":
+
+                    # Easier to read every information
+                    jsonString[regex2].append({
+                        'username': player_name[2],
+                        'round_nr': roundNr.split()[0],
+                        'moves-time': j['title'],
+                        'color': 'Yellow'
+                    })
+                    # print(f"Player: {player_name[2]}|{color_parser(j.span['style'])}|\n\t Moves:\n\t{j['title']}")
+
+                else:
+                    # Easier to read every information
+                    jsonString[regex2].append({
+                        'username': player_name[3],
+                        'round_nr': roundNr.split()[0],
+                        'moves-time': j['title'],
+                        'color': 'Green'
+                    })
+                # print(f"Player: {player_name[3]}|{color_parser(j.span['style'])}|\n\t Moves:\n\t{j['title']}")
+    finally:
+        getChessDb(jsonString)
+    return jsonString
+
 
 USER_NAME = r'TestUserForProject'
 PASSWORD = r'4playerchess'
@@ -33,7 +141,6 @@ try:
         EC.presence_of_all_elements_located((By.XPATH, '//*[@id="sb"]/div[3]/a[8]'))
     )
     element[0].click()
-
     time.sleep(3)
 
 except:
@@ -44,35 +151,4 @@ driver.find_elements(By.XPATH, '//*[@id="password"]')[0].send_keys(PASSWORD)
 driver.find_elements(By.XPATH, '//*[@id="login"]')[0].click()
 time.sleep(5)
 
-#i suggest doing scrapping in test.py at the moment because scrapper is all in one script and every time you run it will open chrome fro chess.com login
-
-with open("data/pages/page.html", "a", encoding="UTF-8") as f:
-    html = driver.page_source
-    #in loop we can accsess directly html in soup
-    # html is messy so i preffered to write it after prettifying it.
-    #f.write(html)
-    soup = BeautifulSoup(html)
-    f.write(soup.prettify())
-
-    for s in soup.select("div.tableRow span[style]"):
-        print(s['style'], s.text)
-
-    #a = soup.find_all("div", {"class": ["fullmoveNr", "pointer"]})
-    #for i in a:
-        #print(i.text)
-    #delete content of page
-    #f.truncate(0)
-
-
-# r = requests.get('http://www.chess.com/members/view/MagnusCarlsen')
-# soup = BeautifulSoup(r.content)
-# for i in soup.find_all('a', href=re.compile("^/livechess/game\?id=")):
-#     print(re.split(r'id=', i['href'])[1])
-
-# page = requests.get(url)
-
-# soup = BeautifulSoup(page.text, 'html.parser')
-
-# print(soup.prettify())
-
-# print(soup.findall(class_='tableCell move'))
+scraper()
