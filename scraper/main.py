b2b0f15bf640c726a24be262ab9df09f4974efd6
RadoRoman <20imc10324@fh-krems.ac.at>
2021-12-09 11:51:06 +0100

    code cleanup, PEP standards

--- scraper/main.py
+++ scraper/main.py
@@ -12,9 +12,9 @@ from SQLite_Db.ChessGameDB import Chess_db
 
 
 def color_parser(string:str) -> str:
-    '''
+    """
     converting rgb values to respective colors for given string.
-    '''
+    """
 
     #predfined colors with rgb values
     yellow = "color: rgb(192, 149, 38)"
@@ -34,16 +34,15 @@ def color_parser(string:str) -> str:
         return "No color specified"
 
 def scraper():
-    '''
+    """
     function to scrape data from given gameNr's html source page using beautifulsoup
     and storing them in Database
-    '''
+    """
     path = os.getcwd()
     x = os.path.abspath(os.path.join(path, os.pardir))
     sys.path.insert(1, x + "\\SQLite_Db")
-    #print(os.getcwd())
 
-    #storing html source page as a variable for further scrapping
+    # storing html source page as a variable for further scrapping
     html = driver.page_source
 
     soup = BeautifulSoup(html, 'html.parser')
@@ -59,24 +58,24 @@ def scraper():
     sql_query_dict[regex2] = []
 
     try:
-        #searching thourgh div section with class attribute "tableRow" which contains information such as roundNr,
-        #colors and moves are located
+        # searching through div section with class attribute "tableRow" which contains information such as roundNr,
+        # colors and moves are located
         for element in soup.find_all("div", {"class": "tableRow"}):
 
-            #parsing elements below for to their respective format
+            # parsing elements below for to their respective format
             roundNr = int(element.div.text.split(".")[0].translate({ord(c): None for c in string.whitespace}))
             color = element.find_all("div", {"class": "pointer"})
             usernames = soup.find_all("div", {"class": "truncate120"})
             player_name = [username.text.split('\n')[1].translate({ord(c): None for c in string.whitespace})
                            for username in usernames]
 
-            #searching thorugh section with class "pointer" where color and move can be found
+            # searching through section with class "pointer" where color and move can be found
             for element in color:
 
-                #matching the correct color using color_parser fucntion
+                # matching the correct color using color_parser fucntion
                 if color_parser(element.span['style']) == "Red":
 
-                    #append the data to query dictionry
+                    # append the data to query dictionry
                     sql_query_dict[regex2].append({
                         'username': player_name[0],
                         'round_nr': roundNr,
@@ -87,7 +86,7 @@ def scraper():
 
                 # matching the correct color using color_parser fucntion
                 elif color_parser(element.span['style']) == "Blue":
-                    #append the data to query dictionry
+                    # append the data to query dictionry
                     sql_query_dict[regex2].append({
                         'username': player_name[1],
                         'round_nr': roundNr,
@@ -98,7 +97,7 @@ def scraper():
 
                 # matching the correct color using color_parser fucntion
                 elif color_parser(element.span['style']) == "Yellow":
-                    #append the data to query dictionry
+                    # append the data to query dictionry
                     sql_query_dict[regex2].append({
                         'username': player_name[2],
                         'round_nr': roundNr,
@@ -108,7 +107,7 @@ def scraper():
                     })
 
                 else:
-                    #append the data to query dictionry
+                    # append the data to query dictionry
                     sql_query_dict[regex2].append({
                         'username': player_name[3],
                         'round_nr': roundNr,
@@ -118,30 +117,30 @@ def scraper():
                     })
 
     finally:
-        #uploading the dictionary to the database
+        # uploading the dictionary to the database
         Chess_db(sql_query_dict)
-    #return the dictionary
+    # return the dictionary
     return sql_query_dict
 
 
 if __name__ == '__main__':
-    #test username and password for login
+    # test username and password for login
     USER_NAME = r'TestUserForProject'
     PASSWORD = r'4playerchess'
 
-    #getting game numbers from txt file an storing in list
+    # getting game numbers from txt file an storing in list
     with open("data/merge_game_nr.txt", "r") as f:
         lines = f.readlines()
         game_nr_list = [line.rstrip() for line in lines]
 
-    #scrapping each game html file using list of game numbers
+    # scrapping each game html file using list of game numbers
     for game_nr in game_nr_list[351:400]:
         driver = webdriver.Chrome(executable_path=r"chromedriver.exe")
-        #common game url with game_nr pasted using loop
+        # common game url with game_nr pasted using loop
         url = "https://www.chess.com/4-player-chess?g={}".format(game_nr)
         driver.get(url)
 
-        #waiting for element to be appeared on screen to be clicked
+        # waiting for element to be appeared on screen to be clicked
         try:
             element = WebDriverWait(driver, 5).until(
                 EC.presence_of_all_elements_located((By.XPATH, '//*[@id="sb"]/div[3]/a[8]'))
@@ -152,11 +151,11 @@ if __name__ == '__main__':
         except:
             driver.quit()
 
-        #passing test login credentials to the browser
+        # passing test login credentials to the browser
         driver.find_elements(By.XPATH, '//*[@id="username"]')[0].send_keys(USER_NAME)
         driver.find_elements(By.XPATH, '//*[@id="password"]')[0].send_keys(PASSWORD)
         driver.find_elements(By.XPATH, '//*[@id="login"]')[0].click()
         time.sleep(4)
-        #call the scrapper fuction after sucessfully logged in
+        # call the scrapper fuction after sucessfully logged in
         scraper()
 
